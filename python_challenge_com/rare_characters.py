1a03b0169f37ebb20d1bc02a11ebab63f0b65a28
Marcos Pereira <marcos.si.pereira@gmail.com>
2016-07-15 15:09:21 -0300

    Find solution on the web

--- python_challenge_com/rare_characters.py
+++ python_challenge_com/rare_characters.py
@@ -1,25 +1,50 @@
 #!/usr/bin/python
 
-from collections import Counter
+# from collections import Counter
 
+# import re
+
+# code = open("rare_characters.txt")
+
+# code_key = ['#','@','!','$','%','+','{','}','[',']','_','-','&','*','(',')','*','^','@','/']
+# decoded_output =""
+
+# # print()
+# # for letter in code:
+# #     if letter not in code_key:
+# #         decoded_output += letter
+# #         print letter       
+
+
+# # count = Counter(code)
+# # s = ''.join(char for char in code if count[char] < 10)
+# # print(s)        
+
+# # decoded_output.strip()
+
+# # print(decoded_output)
+
+# # characters = Counter(code)
+# # print(characters.most_common())
+
+# # message = re.findall("[a-zA-Z]", code)
+# # print message
+
+
+# Solution Reference Level 2: http://garethrees.org/2007/05/07/python-challenge/
+import urllib
+def get_challenge(s):
+     return urllib.urlopen('http://www.pythonchallenge.com/pc/' + s).read()
+src = get_challenge('def/ocr.html')
 import re
+text = re.compile('<!--((?:[^-]+|-[^-]|--[^>])*)-->', re.S).findall(src)[-1]
+counts = {}
+for c in text: counts[c] = counts.get(c, 0) + 1
+counts
+print ''.join(re.findall('[a-z]', text))
 
-code = open("rare_characters.txt")
+# Level 3
 
-code_key = "#@!$%+{}[]_-&*()*^@/\n"
-decoded_output =""
-
-print()
-for letter in code:
-    if letter not in code_key:
-        decoded_output += letter
-
-decoded_output.strip()
-
-print(decoded_output)
-
-characters = Counter(code)
-print(characters.most_common())
-
-# message = re.findall("[a-zA-Z]", code)
-# print message
+src = get_challenge('def/equality.html')
+text = ''.join(re.findall('[^A-Z][A-Z]{3}([a-z])[A-Z]{3}[^A-Z]',src))
+print text
