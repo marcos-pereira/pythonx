78a692655b29329424e6dcf4528b8d07fe365775
Marcos Pereira <marcos.si.pereira@gmail.com>
2017-04-16 18:00:33 +0200

    Read user input from cmd line and adjust excepts

--- interpolator/linear_interpolator/linear_interpolator.py
+++ interpolator/linear_interpolator/linear_interpolator.py
@@ -23,7 +23,7 @@ Created on Tue Fev 2017 21:24:35 2017
 #-------------------------------------------------------------------------------
 import matplotlib.pyplot as plt
 import numpy as np
-
+import sys
 plot_all_data = 1; # If equals 1, plot all data
 
 
@@ -34,6 +34,10 @@ class EmptyFile(RuntimeError):
    def __init__(self, arg):
 	  self.args = arg
 
+class e(RuntimeError):
+	def __init__(self,arg):
+		self.args = arg
+		
 #-------------------------------------------------------------------------------
 # Data file name
 #-------------------------------------------------------------------------------
@@ -43,10 +47,30 @@ data_file_extension = ".txt";
 data_file = data_file_name + "_" + data_file_number + data_file_extension;
 
 #-------------------------------------------------------------------------------
+# User cmd line input arguments
+#-------------------------------------------------------------------------------
+try:
+	print "Reading input parameters...";	
+	if (sys.argv[1] is not None) and (sys.argv[2] is not None):		
+		print "Input parameters found.";		
+		data_file = sys.argv[1];
+		print "Data will be read from file " + data_file + ".";
+		input_argument_1_interpolation_steps = int(sys.argv[2]);
+		print "Number of steps to interpolate is " + sys.argv[2];			
+		pass		
+except:	
+	print "Input parameters are missing!";
+	print "Expect 2 input parameters: <arg1: datafile name.txt> <arg2: number of steps to interpolate>.";
+	sys.exit(0);
+else:
+	pass
+
+#-------------------------------------------------------------------------------
 # Output data file name including interpolated values
 #-------------------------------------------------------------------------------
 output_data_file_extension = ".txt";
-output_data_file_name = data_file_name + \
+output_data_file_name = \
+data_file_name + \
 "_" + data_file_number + \
 "_interpolation_result" + \
 output_data_file_extension;
@@ -54,33 +78,62 @@ output_data_file_extension;
 #-------------------------------------------------------------------------------
 # Read data file and store it into array
 #-------------------------------------------------------------------------------
-try:
-	open(data_file) is not None
-except Exception, e:
+try:	
+	print "Opening input data file " + data_file + "...";
+	open(data_file) is not None	
+except:
 	print "File " + data_file + " not found!" + "\n";
-else:	
+	sys.exit(0);
+else:
+	print "Reading data from input file " + data_file + "...";		
 	with open(data_file) as file_holder:
 		array = []
 		for line in file_holder: # read rest of lines
 			array.append([float(data_value) for data_value in line.split()])				
 	file_data = [row[0] for row in array]
 	file_data_len = len(file_data);
+	
+try:		
+	if len(file_data) > 0:		
+		print str(file_data_len) + " values were counted in " + data_file + ".";		
+		print "Data from file " + data_file + " read."		
+	else:
+		raise (EmptyFile("Input data file " + data_file + " is empty!"));
+except Exception, empty_data_file:		
+	print str(file_data_len) + " values were found in " + data_file + ".";
+	print "Input data file " + data_file + " is empty!";		
+	sys.exit(0);
+# else:			
+# 	print "NOT EMPTZ"
+# 	#print str(file_data_len) + " values were counted in " + data_file + ".";
+# 	print "Data from file " + data_file + " read."
+# 	pass	
 
 #-------------------------------------------------------------------------------
 # Do linear interpolation
 # Reference: https://support.microsoft.com/en-au/help/214096/method-to-calculate-interpolation-step-value-in-excel
 #-------------------------------------------------------------------------------
-try:
-	if file_data_len == 0: raise(EmptyFile("Data file " + data_file + " is empty!"));
-except Exception, empty_data_file:
+try:	
+	if file_data_len != 0: 
+		print "Interpolating " + str(input_argument_1_interpolation_steps) + " values between each input value...";				
+		pass
+except:	
 	print "Data file " + data_file + " is empty!";
+	sys.exit(0);
 else:
 	derivative_rate = [];
 	interpolated_values = [];
-	steps_number_to_add = 3;
+	steps_number_to_add = input_argument_1_interpolation_steps; # Number of steps to be added between each given data value
+
+	#--------------------------------------------------------
+	# Calculate derivative rates
+	#--------------------------------------------------------
 	for ii in xrange(0, file_data_len-1):
 		derivative_rate.append((file_data[ii+1] - file_data[ii])/(steps_number_to_add+1));		
 	
+	#--------------------------------------------------------
+	# Calculate interpolated values using the derivative rates
+	#--------------------------------------------------------
 	for ii in xrange(0, file_data_len-1):
 		interpolated_values.append(file_data[ii]);
 		print "ii: " + str(ii);
@@ -90,7 +143,9 @@ else:
 			interpolation_result = interpolated_values[jj]+derivative_rate[ii];
 			interpolated_values.append(interpolation_result);
 			print "jj: " + str(jj) + ", res: " + str(interpolation_result);		
-	interpolated_values.append(file_data[len(file_data)-1]); # Add the last value									
+	if len(file_data) > 0:
+		interpolated_values.append(file_data[len(file_data)-1]); # Add the last value	
+	print "Interpolation done.";								
 	print "Input data:"
 	print file_data;
 	print "Derivative rates:"
@@ -105,10 +160,11 @@ else:
 #-------------------------------------------------------------------------------
 try:
 	if file_data_len == 0: raise(EmptyFile("Data file " + data_file + " is empty!"));
-except Exception, empty_data_file:
+except:
 	print "Data file " + data_file + " is empty!";
+	sys.exit(0);
 else:	
-	print "Plotting data from file...";		
+	print "Plotting input data from file and interpolated values...";		
 	plt.plot(interpolated_values, interpolated_values, "bx");
 	plt.plot(file_data, file_data, "ro")
 	plt.ylabel('yaxis')
@@ -124,6 +180,7 @@ try:
 	output_file_try_create = open(output_data_file_name,'w') is not None;
 except Exception, empty_data_file:
 	print "Data file " + output_data_file_name + " could not be created!";
+	sys.exit(0);
 else:	
 	print "Creating output file " + output_data_file_name + "... " + str(output_file_try_create) + ".";
 	output_file_ = open(output_data_file_name,'w');
